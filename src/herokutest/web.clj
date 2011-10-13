94796825fd679a4ac336610e13af7ac5650ec1af
Benjamin Peter <benjaminpeter@arcor.de>
2011-10-13 23:40:53 +0200

    added some agent action

--- src/herokutest/web.clj
+++ src/herokutest/web.clj
@@ -4,7 +4,14 @@
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
-   :body "Hello, Ben."})
+; currently useless, no cross request store yet
+   :body (str "Hello, Ben. Visitor: " (let [counter (agent 0)]
+       (send counter inc)
+       (await-for 100 counter)
+       (deref counter))
+     )
+  }
+)
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
