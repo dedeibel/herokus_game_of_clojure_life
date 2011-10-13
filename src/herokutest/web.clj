c9ecc77b153a8e981adba9f1dc94c24070bf5ee0
Benjamin Peter <benjaminpeter@arcor.de>
2011-10-14 00:28:59 +0200

    fixed counter

--- src/herokutest/web.clj
+++ src/herokutest/web.clj
@@ -1,15 +1,12 @@
 (ns herokutest.web
   (:use ring.adapter.jetty))
 
+(def counter (atom 0))
 (defn app [req]
   {:status 200
    :headers {"Content-Type" "text/plain"}
 ; currently useless, no cross request store yet
-   :body (str "Hello, Ben. Visitor: " (let [counter (agent 0)]
-       (send counter inc)
-       (await-for 100 counter)
-       (deref counter))
-     )
+   :body (str "Hello, Ben. Visitor: " (swap! counter inc))
   }
 )
 
