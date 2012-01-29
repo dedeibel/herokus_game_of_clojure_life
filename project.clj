08deec7cd7a00c6d97e8039337d2ce70b9bfd6d8
Benjamin Peter <benjaminpeter@arcor.de>
2012-01-29 22:06:58 +0100

    Added game of life implementation as output.

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject herokutest "0.0.1"
-  :dependencies
-    [[org.clojure/clojure "1.2.1"]
-     [ring/ring-jetty-adapter "0.3.9"]])
+  :dependencies [
+    [org.clojure/clojure "1.3.0"]
+    [ring/ring-jetty-adapter "0.3.9"]
+    [game_of_life "1.0.0-SNAPSHOT"]
+  ]
+  :repositories {
+    "local" ~(str (.toURI (java.io.File. "repo")))
+  }
+)
--- src/herokutest/web.clj
+++ src/herokutest/web.clj
@@ -1,15 +1,49 @@
 (ns herokutest.web
-  (:use ring.adapter.jetty))
+  (:use [game_of_life.cell :only (new_cell)])
+  (:use [game_of_life.world_printer :only (to_string)])
+  (:use [game_of_life.world_builder :only (from_string)])
+  (:use [game_of_life.game :only (next_generation)])
+  (:use ring.adapter.jetty)
+)
+
+(def twentyfour_and_more
+"xx
+,xx
+,
+,
+,
+,                                   x x  xxx
+,                                   x x    x
+,                                   xxx  xxx
+,                                     x  x
+,                                     x  xxx
+,                                                                                                   xx  
+,                                                                                                  x  x
+,                                                                                                 x    x
+,                                                                                                x      x
+,                                                                                                x      x
+,                                                                                                 x    x
+,                                                                                                  x  x
+,                                                                                                   xx"
+)
 
 (def counter (atom 0))
+(def world (atom (from_string twentyfour_and_more new_cell)))
+
 (defn app [req]
-  {:status 200
-   :headers {"Content-Type" "text/plain"}
-; currently useless, no cross request store yet
-   :body (str "Hello, Ben. Visitor: " (swap! counter inc))
-  }
+  (let [current @world]
+    (swap! counter inc)
+    (swap! world next_generation)
+    {:status 200
+     :headers {"Content-Type" "text/plain"}
+     :body 
+       (str "Game Of Life (Step " @counter "):\n" (to_string current))
+    }
+  )
 )
 
 (defn -main []
   (let [port (Integer/parseInt (System/getenv "PORT"))]
     (run-jetty app {:port port})))
+
+
